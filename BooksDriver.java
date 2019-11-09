f9223473eadae51356ae1faa2638460e935bd2d7
Zoryna <jsoryna@gmail.com>
2019-11-09 15:59:33 -0600

    User can add query to link

--- BooksDriver.java
+++ BooksDriver.java
@@ -1,5 +1,6 @@
 /*
-/*
+Coded by Janeen Soria
+
 - Type in a query and display a list of 5 books matching that query. (HTTP GET)
 - Each item in the list should include the book's author, title, and publishing company. (HTTP GET)
 
@@ -7,29 +8,52 @@
 - View a "Reading List" with all the books the user has selected from their queries -- this is a local reading list and not tied to Google Books's account features.
 */
 
-
+import java.util.Scanner;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
 
 public class BooksDriver
 {
   public static void main(String args[]) throws IOException {
 
-    URL url = new URL("https://api.github.com/users/google"); //testing
-    HttpURLConnection con = (HttpURLConnection) url.openConnection();
-    con.setRequestMethod("GET");
+    //AIzaSyAI5Pn4IbnRRrHolRJ2SKGO2eHByl7Ua4I (API key)
 
-    BufferedReader in = new BufferedReader(new InputStreamReader(
-        con.getInputStream()));
+    Scanner keyboard = new Scanner (System.in);
+    String q = keyboard.nextLine().toLowerCase(); //user enters query
+    String link = "https://www.googleapis.com/books/v1/volumes?q=" + URLEncoder.encode(q, "UTF-8"); //query is added to url
+    URL url = new URL(link); //url now with query
+
+    HttpURLConnection con = (HttpURLConnection) url.openConnection(); //the actual connection
+    con.setRequestMethod("GET"); //the type of request-getting information from the API
+
+    //BufferedReader and InputStreamReader that allows characters to be readable
+    //parameter is the connection getting the data
+
+    BufferedReader bufferR = new BufferedReader(new InputStreamReader(con.getInputStream()));
     String inputLine;
-    while ((inputLine = in.readLine()) != null) {
-      System.out.println(inputLine);
+    while ((inputLine = bufferR.readLine()) != null) //while inputLine that equals the buffer reading the text IS NOT null
+    {
+      System.out.println(inputLine); //print the data
     }
 
-    in.close();
+    bufferR.close(); //closes the stream that is releasing the resources
+
+
+
+
+
+
+
+
+
+
+
   }
 }
